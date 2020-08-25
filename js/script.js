faf3a477c802760f24b73bf8da7e8be29bdeb06e
Marc Sanchez <marcsanchez@me.com>
2020-08-25 09:35:47 -0400

    updated hero img

--- js/script.js
+++ js/script.js
@@ -3,5 +3,24 @@ setTimeout(function() {
   $('header').slideDown(1000);
 }, 1000);
 
+const navbar = document.getElementsByClassName('navbar');
+    let scrolled = false;
+
+    window.onscroll = function() {
+      if(window.pageYOffset > 100) {
+        navbar.classList.remove('top');
+        if (!scrolled) {
+          navbar.style.transform = 'translateY(-70px)';
+        }
+        setTimeout(function() {
+          navbar.style.transform = 'translate(0)';
+          scrolled = true;
+        }, 200);
+      } else {
+        navbar.classList.add('top');
+        scrolled = false;
+      }
+    }
+
 
 
